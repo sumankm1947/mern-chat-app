1bff333e6f1b535cc277b4b3b1a3b074e4268d56
superbsuman <sumanmandal.superb@gmail.com>
2022-08-31 15:27:54 +0530

    socket cors origin set to all

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const server = app.listen(PORT, console.log(`Server started on port ${PORT}`));
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:5000",
+    origin: "*",
   },
 });
 
